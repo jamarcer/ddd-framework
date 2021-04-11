d5a8956ccd831d25c11df59a538a89bbe6f70eef
Jesus Martinez <jamartinez@undanet.com>
2021-04-11 13:07:55 +0200

    Scope processing functionality.

--- src/Domain/Model/DomainEvent.php
+++ src/Domain/Model/DomainEvent.php
@@ -7,8 +7,19 @@ use Jamarcer\DDD\Util\Message\AggregateMessage;
 
 abstract class DomainEvent extends AggregateMessage
 {
+    protected const DEFAULT_SCOPE = 'world';
+    protected string $scope;
+
     final public static function messageType(): string
     {
         return 'domain_event';
+    }
+
+    protected function getScope(array $payload): void
+    {
+        $this->scope = self::DEFAULT_SCOPE;
+        if (array_key_exists('scope', $payload)) {
+            $this->scope = $payload['scope'];
+        }
     }
 }
